6cd7622cc2b9222386e97c9aeb4680f3593162fe
ikolumbova <ikolumbova@applicationcraft.com>
2013-10-16 15:08:50 +0400

    first

---
+++ Resources/xNzconfig.js
@@ -0,0 +1,16 @@
+AC.Config.set({
+    apiAdapter : "Web",
+    needFlashVersion : "9.0.0",
+    clientApi : "https://lessstress.applicationcraft.com/",
+    serverUrl : "https://lessstress.applicationcraft.com/service/",
+    serverUploadUrl : "https://lessstress.applicationcraft.com/service/",
+    uploadServerPath: "https://lessstress.applicationcraft.com/service/uploads/",
+    uploadServerDirectPath: "https://lessstress.applicationcraft.com/service/uploads/direct.php",
+    serverEventsUrl: 'https://notify.applicationcraft.com/events',
+    formatApi : "json",
+    serverThemes: "themes/",
+    localAppsPath: 'Apps/',
+    flashAdapterUrl : "wiziCore/api/server/adapter/WiziSendRequest.swf",
+    isDebugRequest : true,
+    googleApiKey: {"rows":[{"id":1,"data":["http://applicationcraft.com/","ABQIAAAACfMroKb5WzfdVTHbQi2OfRRGQFuqEDC-bwWLPZ60Ces-G56XXhTiOZYFBXlh0_EId2nXJCwq7iTNIg"],"ind":1}]}
+});
